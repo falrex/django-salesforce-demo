6e320758fcf74ce06eb8e7ddf7b2c8df6d6e0269
Dennis Dominic Diego <diego.dennisdominic@gmail.com>
2015-10-28 21:22:34 -0700

    serving static files

--- myproject/settings.py
+++ myproject/settings.py
@@ -125,7 +125,9 @@ ALLOWED_HOSTS = ['*']
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = 'http://workpipes.com/static/' #PRODUCTION VALUE
+STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -4,5 +4,8 @@ import sys
 os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
